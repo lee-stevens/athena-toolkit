39ed72d1579fa129b3764a8d1f6caa03de360963
Lee Stevens <lee.stevens.dev@proton.me>
2025-02-21 19:53:19 +0000

    Downgraded Angular a few versions as well as PrimeNG until the 19.0.6 bug is fixed in primeng 19.0.7; Added a css app to compare with the scss app with tailwind 4 vs primeNG because apparently tailwind 4 shouldnt work with scss

--- athena-client/projects/athena-core/src/app/app.component.ts
+++ athena-client/projects/athena-core/src/app/app.component.ts
@@ -1,18 +1,94 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CheckboxModule } from 'primeng/checkbox';
+import { ChartModule } from 'primeng/chart';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    // CheckboxModule,
-    // ButtonModule
+    CheckboxModule,
+    ButtonModule,
+    ChartModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'athena-core';
+
+  basicData: any;
+  basicOptions: any;
+  platformId = inject(PLATFORM_ID);
+
+  constructor(
+    private cd: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.initChart();
+  }
+
+  toggleTheme(): void {
+
+  }
+
+  initChart() {
+      if (isPlatformBrowser(this.platformId)) {
+          const documentStyle = getComputedStyle(document.documentElement);
+          const textColor = documentStyle.getPropertyValue('--p-text-color');
+          const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+          const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+          this.basicData = {
+              labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+              datasets: [
+                  {
+                      label: 'Sales',
+                      data: [540, 325, 702, 620],
+                      backgroundColor: [
+                          'rgba(249, 115, 22, 0.2)',
+                          'rgba(6, 182, 212, 0.2)',
+                          'rgb(107, 114, 128, 0.2)',
+                          'rgba(139, 92, 246, 0.2)',
+                      ],
+                      borderColor: ['rgb(249, 115, 22)', 'rgb(6, 182, 212)', 'rgb(107, 114, 128)', 'rgb(139, 92, 246)'],
+                      borderWidth: 1,
+                  },
+              ],
+          };
+
+          this.basicOptions = {
+              plugins: {
+                  legend: {
+                      labels: {
+                          color: textColor,
+                      },
+                  },
+              },
+              scales: {
+                  x: {
+                      ticks: {
+                          color: textColorSecondary,
+                      },
+                      grid: {
+                          color: surfaceBorder,
+                      },
+                  },
+                  y: {
+                      beginAtZero: true,
+                      ticks: {
+                          color: textColorSecondary,
+                      },
+                      grid: {
+                          color: surfaceBorder,
+                      },
+                  },
+              },
+          };
+          this.cd.markForCheck()
+      }
+  }
 }
