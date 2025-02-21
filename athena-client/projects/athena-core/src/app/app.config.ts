62c490da3e7ff7f28123f36257f781b0a0b2b187
Lee Stevens <lee.stevens@atomus.com>
2025-02-21 16:53:16 +0000

    Adding PrimeNG with Aura theme preset;

--- athena-client/projects/athena-core/src/app/app.config.ts
+++ athena-client/projects/athena-core/src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
@@ -7,6 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    providePrimeNG({ theme: 'aurora'})
+    provideAnimationsAsync(),
+    providePrimeNG({ theme: { preset: Aura }})
   ]
 };
