dc832d93a03d20d01169a94fdad0902e3918b5d5
Lee Stevens <lee.stevens.dev@proton.me>
2025-02-22 10:57:20 +0000

    Component rename;

--- athena-client/projects/athena-core/src/app/@features/dev-space/dev-space.component.ts
+++ athena-client/projects/athena-core/src/app/@features/dev-space/dev-space.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { TailwindComponent } from './tailwind/tailwind.component';
 import { PrimengComponent } from './primeng/primeng.component';
 import { MiscComponent } from './misc/misc.component';
-import { PrimeChartsComponent } from './charts/prime-charts.component';
+import { PrimeChartsComponent } from './prime-charts/prime-charts.component';
 import { TabsModule } from 'primeng/tabs';
 import { DevDashboardComponent } from './dev-dashboard/dev-dashboard.component';
 
